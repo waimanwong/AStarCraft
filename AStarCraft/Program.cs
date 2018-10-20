b1cee1d0fb3bbf36c248bd2dfe98fc47906a634c
wwong <wwong@lucca.fr>
2018-10-20 14:59:45 +0200

    Handle deadends

--- AStarCraft/Program.cs
+++ AStarCraft/Program.cs
@@ -12,6 +12,30 @@ struct Position
         this.x = x;
         this.y = y;
     }
+    
+    //Return top, bottom, left, right
+    public Position[] GetNeighbors()
+    {
+        Position[] neighbors = new Position[4];
+
+        //top
+        int yTop = this.y == 0 ? Map.Height - 1 : this.y - 1;
+        neighbors[0] = new Position(this.x, yTop);
+
+        //bottom
+        int yBottom = this.y == Map.Height - 1 ? 0 : this.y + 1;
+        neighbors[1] = new Position(this.x, yBottom);
+
+        //Left
+        int xLeft = this.x == 0 ? Map.Width - 1 : this.x - 1;
+        neighbors[2] = new Position(xLeft, this.y);
+
+        //Right
+        int xRight = this.x == Map.Width ? 0 : this.x + 1;
+        neighbors[3] = new Position(xRight, this.y);
+
+        return neighbors;
+    }
 }
 
 class Map
@@ -20,12 +44,23 @@ class Map
     public const int Height = 10;
     public const int Width = 19;
 
-
+    public const char Void = '#';
+    public const char Empty = '.';
+    public const char UpArrow = 'U';
+    public const char RightArrow = 'R';
+    public const char DownArrow = 'D';
+    public const char LeftArrow = 'L';
+    
     public Map(string[] rows)
     {
         this.cells = rows.Select(row => row.ToCharArray()).ToArray();
     }
     
+    public char GetCell(Position pos)
+    {
+        return GetCell(pos.x, pos.y);
+    }
+
     public char GetCell(int x, int y)
     {
         return cells[y][x];
@@ -49,8 +84,9 @@ class Map
         }
         return _platformCells.ToArray();
     }
-    
-    public Map Apply(List<Arrow> arrows)
+
+
+    public Map Apply(IEnumerable<Arrow> arrows)
     {
         var newMap = (Map)this.MemberwiseClone();
         foreach(var arrow in arrows)
@@ -74,15 +110,28 @@ struct Arrow
 {
     public int x, y;
     public char direction;
-    public Arrow(int x, int y, char direction)
+    public Arrow(Position p, char direction)
     {
-        this.x = x;
-        this.y = y;
+        this.x = p.x;
+        this.y = p.y;
         this.direction = direction;
     }
     public override string ToString()
     {
         return $"{x.ToString()} {y.ToString()} {direction}";
+    }
+}
+
+struct RobotState
+{
+    public int x;
+    public int y;
+    public char direction;
+    public RobotState(int x, int y, char direction)
+    {
+        this.x =x;
+        this.y = y;
+        this.direction = direction;
     }
 }
 
@@ -104,10 +153,10 @@ class Robot
         return new Robot(x, y, direction);
     }
 
-    
+
     public void Move()
     {
-        switch(direction)
+        switch (direction)
         {
             case 'U':
                 y = y == 0 ? Map.Height - 1 : y - 1;
@@ -124,6 +173,10 @@ class Robot
         }
     }
 
+    public RobotState DumpState()
+    {
+        return new RobotState(this.x, this.y, this.direction);
+    }
 }
 
 static class ScoreCalculator
@@ -141,112 +194,124 @@ static class ScoreCalculator
 
     private static int ComputeScore(Map map, Robot robot)
     {
+        ChangeDirectionIfLocatedOnAnArrow(map, robot);
+
         bool robotIsAlive = true;
         int score = 0;
-        HashSet<int> visitedArrows = new HashSet<int>();
+        HashSet<RobotState> visitedStates = new HashSet<RobotState>(1000);
 
-        visitedArrows.Add(robot.y * Map.Width + robot.x);
+        visitedStates.Add(robot.DumpState());
 
-        while (robotIsAlive )
+        while (robotIsAlive)
         {
+            //Score is incremented by the number of robots in function.
             score++;
 
+            //Automaton2000 robots move by 1 cell in the direction they're facing.
             robot.Move();
 
-            var cellContent = map.GetCell(robot.x, robot.y);
-            switch(cellContent)
-            {
-                case '#':
-                    robotIsAlive = false;
-                    break;
-                case '.':
-                    break;
-                case 'U':
-                case 'R':
-                case 'D':
-                case 'L':
-                    int hashedArrow = robot.y * Map.Width + robot.x;
+            //Automaton2000 robots change their direction if they're located on an arrow.
+            ChangeDirectionIfLocatedOnAnArrow(map, robot);
 
-                    if (visitedArrows.Contains(hashedArrow))
-                    {
-                        robotIsAlive = false;
-                    }
-                    else
-                    {
-                        visitedArrows.Add(hashedArrow);
-                        robot.direction = cellContent;
-                    }
-                    break;
+            //Automaton2000 robots stop functioning if they're located on a void cell or if they've entered a state(position, direction) they've been in before. (Automaton2000 robots don't share their state history)
+            var robotState = robot.DumpState();
+            var cellContent = map.GetCell(robot.x, robot.y);
+            if (cellContent == Map.Void || visitedStates.Contains(robotState))
+            {
+                robotIsAlive = false;
             }
+            else
+            {
+                visitedStates.Add(robotState);
+            }
+
         }
 
         return score;
     }
+
+    private static void ChangeDirectionIfLocatedOnAnArrow(Map map, Robot robot)
+    {
+        //Automaton2000 robots change their direction if they're located on an arrow.
+        var cellContent = map.GetCell(robot.x, robot.y);
+        switch (cellContent)
+        {
+            case 'U':
+            case 'R':
+            case 'D':
+            case 'L':
+                robot.direction = cellContent;
+                break;
+            default:
+                //Do nothing
+                break;
+        }
+    }
 }
 
-static class SolutionOptimizer
+class Solution
 {
+    private List<Arrow> _arrowsToPlace = new List<Arrow>();
+
+    public Arrow[] Arrows => _arrowsToPlace.ToArray();
     
-    public static Tuple<List<Arrow>, int> FindBestSolution(Map map, Robot[] robots)
+    public void Add(Arrow arrow)
     {
-        var emptyCells = map.GetPlatformCells();
-        int bestScore = 0;
-        List<Arrow> bestSolution = new List<Arrow>();
-
-        Stopwatch stopwatch = Stopwatch.StartNew();
-
-        while(stopwatch.ElapsedMilliseconds < 900)
-        {
-            var solution = BuildSolution(emptyCells);
-
-            var newMap = map.Apply(solution);
-
-            var score = ScoreCalculator.ComputeScore(newMap, robots.Select(r => r.Clone()).ToArray());
-            if(score > bestScore)
-            {
-                bestScore = score;
-                bestSolution = solution;
-            }
-        }
-
-        return new Tuple<List<Arrow>, int>(bestSolution, bestScore);
+        this._arrowsToPlace.Add(arrow);
     }
 
-    private static List<Arrow> BuildSolution(Position[] emptyCells)
+    public override string ToString()
     {
-        List<Arrow> solution = new List<Arrow>(emptyCells.Length);
-        Random rand = new Random();
+        return string.Join(" ", this._arrowsToPlace.Select(arrow => arrow.ToString()).ToArray());
+    }
+}
 
-        for(int i = 0; i < emptyCells.Length; i++)
+static class SolutionFinder
+{
+    
+    public static Solution FindBestSolution(Map map, Robot[] robots)
+    {
+        var platformCells = map.GetPlatformCells();
+        var solution = new Solution();
+        
+        
+        //Dead ends
+        foreach(var platformCell in platformCells)
         {
-            var currentCell = emptyCells[i];
+            var neighbors = platformCell.GetNeighbors();
+            var neighborCells = neighbors.Select(neighborPosition => map.GetCell(neighborPosition)).ToArray();
 
-            //Place a arrow or not ?
-            if(rand.NextDouble() < 0.5)
+            if(neighborCells.Count(c => c == '#') == 3)
             {
-                //Which arrow ?
-                var randomArrow = rand.NextDouble();
-                if(randomArrow <= 0.25)
+                //Return top
+                if(map.GetCell(neighbors[0]) == Map.Empty)
                 {
-                    solution.Add(new Arrow(currentCell.x, currentCell.y, 'U'));
+                    solution.Add(new Arrow(platformCell, Map.UpArrow));
                 }
-                else if(randomArrow <= 0.5)
+                //Return bottom
+                if (map.GetCell(neighbors[1]) == Map.Empty)
                 {
-                    solution.Add(new Arrow(currentCell.x, currentCell.y, 'R'));
+                    solution.Add(new Arrow(platformCell, Map.DownArrow));
                 }
-                else if (randomArrow <= 0.75)
+                //Return left
+                if (map.GetCell(neighbors[2]) == Map.Empty)
                 {
-                    solution.Add(new Arrow(currentCell.x, currentCell.y, 'D'));
+                    solution.Add(new Arrow(platformCell, Map.LeftArrow));
                 }
-                else
+                //Return right
+                if (map.GetCell(neighbors[3]) == Map.Empty)
                 {
-                    solution.Add(new Arrow(currentCell.x, currentCell.y, 'L'));
+                    solution.Add(new Arrow(platformCell, Map.RightArrow));
                 }
             }
         }
+        var newMap = map.Apply(solution.Arrows);
+        int score = ScoreCalculator.ComputeScore(newMap, robots);
+        Player.Debug($"Expected score {score.ToString()}");
 
         return solution;
     }
+
 
 }
 
@@ -281,15 +346,13 @@ class Player
 
             robots[i] = new Robot(x, y, direction[0]);
         }
-
-        var bestSolution = SolutionOptimizer.FindBestSolution(map, robots);
-        Player.Debug($"Expected score {bestSolution.Item2.ToString()}");
-
+        
+        var bestSolution = SolutionFinder.FindBestSolution(map, robots);
+        
+       
         // Write an action using Console.WriteLine()
         // To debug: Console.Error.WriteLine("Debug messages...");
-
-        var output = string.Join(" ", bestSolution.Item1.Select(arrow => arrow.ToString()).ToArray());
         
-        Console.WriteLine(output);
+        Console.WriteLine(bestSolution.ToString());
     }
 }
